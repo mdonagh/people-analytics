fd843631ee5857288910679b82c75306c7cd9087
Mark Donahue <mdonagh@icloud.com>
2019-12-09 16:37:27 -0800

    permissions

--- config/oauth_environment_variables.rb
+++ config/oauth_environment_variables.rb
@@ -1,3 +1,3 @@
 ENV['AZURE_APP_ID'] = '88ba33dd-0b1e-4f67-80ac-2f6da4f0e353'
 ENV['AZURE_APP_SECRET'] = 'N:0YuFg.oHh.?dBfuOrIQra96HBKw9l0'
-ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read calendars.read analytics.read'
+ENV['AZURE_SCOPES'] = 'user.read analytics.read'
